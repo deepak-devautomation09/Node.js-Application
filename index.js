ceae27438b84a902741f17db1ab39e1a23f2079a
devautomation <deepak_kr92@icloud.com>
2025-04-30 22:08:58 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    const html = '<html><body><h1> Hello World this is my first Node.Js Application Using Jenkins CI\CD  </h1></body></html>';
+    const html = '<html><body><h1> Hello World This is my first Node.Js Application Using Jenkins CI\CD  </h1></body></html>';
     res.end(html);
 });
-app.listen(port, () => console.log(`Hello world app started & listening on port ${port}!`))
+app.listen(port, () => console.log(`Hello World app started & listening on port ${port}!`))
 
 module.exports = app;
